ffc2fc928c97050a51893c16a1eebd3e17592cdf
Vivek Satikunvar <viveksatikunvar142@gmail.com>
2023-03-20 23:58:18 +0530

    test commit

---
+++ next.config.js
@@ -0,0 +1,17 @@
+
+const runtimeCaching = require('next-pwa/cache')
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withPWA({
+  reactStrictMode: true,
+})
+
+module.exports = nextConfig
